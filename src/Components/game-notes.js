67f1d3165e788be9b422fb31dfca22b9f01577f7
Colby Harper <colby.harper11@gmail.com>
2022-06-24 12:30:43 -0500

    how to play template

--- src/Components/game-notes.js
+++ src/Components/game-notes.js
@@ -10,9 +10,21 @@ const GameNotes = ({toggleNotes}) => {
     return (
         <div className="modal">
             <div className="modal_content">
-                <div className='content-list'><h2>Test</h2></div>
-                <div className='content-list'><h2>Test</h2></div>
-                <div className='content-list'><h2>Test</h2></div>
+                <div className='content-list'>
+                    <h2>How to Play</h2>
+                </div>
+                <div className='content-list'>
+                    <h2>Your Craft</h2>
+                    <span>pilot your ship with the arrow keys.</span>
+                </div>
+                <div className='content-list'>
+                    <h2>Operate Plasma Cannon</h2>
+                    <span>fire plasma cannon with s key.</span>
+                </div>
+                <div className='content-list'>
+                    <h2>Items</h2>
+                    <span>destroy item robots to pick up items that can be used for limited time.</span>
+                </div>
                 <div className='content-list'>
                     <button
                         variant="contained"
