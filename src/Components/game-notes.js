de9adcbc3a43e129e19e551defd170eb50a77027
Colby Harper <colby.harper11@gmail.com>
2022-06-24 22:55:57 -0500

    style how to sheet

--- src/Components/game-notes.js
+++ src/Components/game-notes.js
@@ -10,28 +10,52 @@ const GameNotes = ({toggleNotes}) => {
     return (
         <div className="modal">
             <div className="modal_content">
-                <div className='content-list'>
+                <div className='closeBtn'>
+                    <button className='button'
+                        onClick={handleClose}>
+                        Close
+                    </button>
+                </div>
+                <div className='title'>
                     <h2>How to Play</h2>
                 </div>
-                <div className='content-list'>
-                    <h2>Your Craft</h2>
-                    <span>pilot your ship with the arrow keys.</span>
-                </div>
-                <div className='content-list'>
-                    <h2>Operate Plasma Cannon</h2>
-                    <span>fire plasma cannon with s key.</span>
-                </div>
-                <div className='content-list'>
-                    <h2>Items</h2>
-                    <span>destroy item robots to pick up items that can be used for limited time.</span>
-                </div>
-                <div className='content-list'>
-                    <button
-                        variant="contained"
-                        color="primary"
-                        onClick={handleClose}>
-                        Done
-                    </button>
+                <div className='table'>
+                    <div className='cell'>
+                        <div className='infoRow'>
+                            <h3 className='title'>Your Craft</h3>
+                            <img src="DisplayImages/craft.png"/>
+                            <h3 className='title'>Force</h3>
+                            <img src="DisplayImages/forceImg.png"/>
+                        </div>
+                        <li>pilot your ship with the arrow keys</li>
+                        <li>force is indestructible diamond armor with multiple weapon capability</li>
+                        <h3 className='title'>Operate Plasma Cannon</h3>
+                        <li>fire plasma cannon with <b>s</b> key.</li>
+                        <li>Hold <b>s</b> key to accumulate plasma beam energy (indicated by meter)</li>
+                        <li>Release key to fire a powerful stored energy plasma beam</li>
+                    </div>
+                    <div className='cell'>
+                        <h3 className='title item-title'>Items</h3>
+                        <p>destroy item robots to pick up items that can be used for limited time</p>
+                        <div className='items'>
+                            <div className='infoRow'>
+                                <img src="DisplayImages/shieldImgx2.png"/>
+                                <span>diamond shield</span>
+                            </div>
+                            <div className='infoRow'>
+                                <img src="DisplayImages/item.png"/>
+                                <span>weapon upgrade</span>
+                            </div>
+                            <div className='infoRow'>
+                                <img src="DisplayImages/speedImg.png"/>
+                                <span>speed boost</span>
+                            </div>
+                        </div>
+                        <div className='items item-title'>
+                            <img src="DisplayImages/itemRobot.png"/>
+                            <p>item robot</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
